1ec02c8bf360e4fbbbcf64cb6b6fe25347d1ee56
Javier <jrh310392@hotmail.com>
2018-07-26 16:42:48 +0200

    Añadido script de recorridos

---
+++ sara_2dnav/script/recorrido_terrazav2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import rospy
+
+import math
+from math import sin, cos, pi
+
+import tf
+
+# Brings in the SimpleActionClient
+import actionlib
+# Brings in the .action file and messages used by the move base action
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def movebase_client():
+
+   # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+
+   # Waits until the action server has started up and started listening for goals.
+    client.wait_for_server()
+
+   # Goal1
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position.x = 9.776
+    goal.target_pose.pose.position.y = 5.47
+
+    goal.target_pose.pose.orientation.x = 0.0
+    goal.target_pose.pose.orientation.y = 0.0
+    goal.target_pose.pose.orientation.z = 0.5079
+    goal.target_pose.pose.orientation.w = 0.8614
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+    # Waits for the server to finish performing the action.
+    wait = client.wait_for_result()
+
+    #Goal2
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position.x = 23.0281
+    goal.target_pose.pose.position.y = 20.953
+
+    goal.target_pose.pose.orientation.x = 0.0
+    goal.target_pose.pose.orientation.y = 0.0
+    goal.target_pose.pose.orientation.z = 0.273
+    goal.target_pose.pose.orientation.w = 0.96201
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+    # Waits for the server to finish performing the action.
+    wait = client.wait_for_result()
+
+    
+
+
+   # If the result doesn't arrive, assume the Server is not available
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+    # Result of executing the action
+        return client.get_result()
+
+# If the python node is executed as main process (sourced directly)
+if __name__ == '__main__':
+    try:
+       # Initializes a rospy node to let the SimpleActionClient publish and subscribe
+        rospy.init_node('movebase_client_py')
+        result = movebase_client()
+        if result:
+            rospy.loginfo("Goal execution done!")
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation test finished.")
